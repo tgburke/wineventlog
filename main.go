8febe5840f3dc63b0a299873bd4aad289f8cca81
Burke <tony.burke@solarwinds.com>
2019-06-26 18:13:26 -0500

    add reuse EventLog option

--- main.go
+++ main.go
@@ -15,9 +15,6 @@ func main() {
 }
 
 func getLogs() {
-	var states map[string]checkpoint.EventLogState
-	states = make(map[string]checkpoint.EventLogState)
-
 	channels := []string{"System", "Application", "Security"} // Security requires elevated priviliges
 	var configs []*common.Config
 	for _, channel := range channels {
@@ -31,16 +28,22 @@ func getLogs() {
 			"batch_read_size": 100,
 		})
 		configs = append(configs, config)
-		states[channel] = checkpoint.EventLogState{}
 	}
 
+	getLogsFromConfigReuseLog(configs)
+}
+
+func getLogsFromConfigNoReuseLog(configs []*common.Config) {
 	var wg sync.WaitGroup
 	wg.Add(len(configs))
-
+	var states map[string]checkpoint.EventLogState
+	states = make(map[string]checkpoint.EventLogState)
 	for _, config := range configs {
+		channel, _ := config.String("name", 0)
+		states[channel] = checkpoint.EventLogState{}
 		go func(config *common.Config, wg *sync.WaitGroup) {
 			defer wg.Done()
-			for { // loop until ctrl-c
+			for { // loop until ctrl-c or error
 				// example of log open/close on each iteration keeping track of the EventLogState on re-open
 				log, err := eventlog.New(config)
 				if err != nil {
@@ -48,6 +51,7 @@ func getLogs() {
 					return
 				}
 
+				// Opening/Closing the EventLog every iteration appears to leak memory
 				if err = log.Open(states[log.Name()]); err != nil {
 					fmt.Println(err)
 					return
@@ -71,6 +75,46 @@ func getLogs() {
 					states[log.Name()] = records[len(records)-1].Offset
 				}
 				log.Close()
+			}
+		}(config, &wg)
+	}
+	wg.Wait()
+}
+
+func getLogsFromConfigReuseLog(configs []*common.Config) {
+	var wg sync.WaitGroup
+	wg.Add(len(configs))
+	for _, config := range configs {
+		go func(config *common.Config, wg *sync.WaitGroup) {
+			defer wg.Done()
+			log, err := eventlog.New(config)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if err = log.Open(checkpoint.EventLogState{}); err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer log.Close()
+
+			for { // loop until ctrl-c or error
+				records, err := log.Read()
+				if err != nil {
+					fmt.Println(err)
+					return
+
+				} else if len(records) == 0 {
+					time.Sleep(10 * time.Second)
+
+				} else {
+					// do something with the Records  ref: https://github.com/elastic/beats/blob/master/winlogbeat/eventlog/eventlog.go#L77
+					for _, record := range records {
+						fmt.Printf("event[%v]\n", record.ToEvent())
+					}
+					fmt.Printf("channel[%s] count[%d]\n\n", log.Name(), len(records))
+				}
 			}
 		}(config, &wg)
 	}
